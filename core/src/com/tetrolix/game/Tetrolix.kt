76a5dde3089b3537e9a616402c2a2900dfaaf32f
Quillraven <herf@gmx.at>
2019-06-09 13:43:47 +0200

    added mobile UI and functionality to gamescreen

--- core/src/com/tetrolix/game/Tetrolix.kt
+++ core/src/com/tetrolix/game/Tetrolix.kt
@@ -23,7 +23,7 @@ import ktx.style.*
 
 const val UI_BTN_SIZE = 75f
 
-// TODO create UI (show highscore, show next block, flash rows that get removed, randomly fill grid when lost, select starting level, save highscore and volume in preference and show it in menu)
+// TODO create UI (flash rows that get removed, randomly fill grid when lost, save highscore and volume in preference and show it in menu)
 class Tetrolix : KtxGame<KtxScreen>() {
     private val context = Context()
 
--- core/src/com/tetrolix/game/screen/GameScreen.kt
+++ core/src/com/tetrolix/game/screen/GameScreen.kt
@@ -6,19 +6,25 @@ import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.utils.viewport.Viewport
 import com.tetrolix.game.*
 import com.tetrolix.game.model.Block
 import com.tetrolix.game.model.ColorTheme
 import com.tetrolix.game.model.Grid
+import ktx.actors.onClick
+import ktx.actors.txt
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.graphics.use
 import ktx.inject.Context
+import ktx.scene2d.*
 
 private const val MAX_RESETS = 20
 private const val ROWS_FOR_NEXT_LEVEL = 10
 private const val MAX_LOCK_TIMER = 0.75f
+private const val GAME_SCALE = 0.72f
+private const val PREVIEW_BLOCK_SIZE = 24f
 
 class GameScreen(context: Context) : KtxScreen {
     private val game = context.inject<KtxGame<KtxScreen>>()
@@ -43,10 +49,10 @@ class GameScreen(context: Context) : KtxScreen {
     private var highscore = 0
     private var lockTimer = 0f
 
+    private val highscoreLabel = Label("Highscore: $highscore", Scene2DSkin.defaultSkin, Labels.BrightBgd())
+    private val levelLabel = Label("Current Level: $currentLevel", Scene2DSkin.defaultSkin, Labels.BrightBgd())
 
     override fun show() {
-        stage.clear()
-
         // set music
         if (currentLevel >= 7) {
             // more epic Music for finale ;)
@@ -64,15 +70,52 @@ class GameScreen(context: Context) : KtxScreen {
         numResets = 0
         clearedRows = 0
         accumulator = 0f
-        highscore = 0
+        updateHighscore(0)
         lockTimer = 0f
 
         // spawn first block
         currentBlock.spawn(grid)
+
+        // create UI
+        stage.clear()
+        stage.addActor(table {
+            // highscore
+            actor(highscoreLabel) { cell -> cell.expandX().width(300f).top().left().padTop(15f).padLeft(10f) }
+            // level indicator
+            actor(levelLabel) { cell -> cell.expandX().width(255f).colspan(2).top().right().padRight(10f).padTop(15f).row() }
+            // next block
+            label("Next block: ", Labels.BrightBgd()) { cell -> cell.expand().width(300f).top().left().padLeft(10f).padTop(5f).colspan(3).row() }
+
+            // rotate block buttons
+            imageButton(Buttons.RotateLeft()) { cell -> cell.size(UI_BTN_SIZE).left().bottom().expand().padLeft(10f).padBottom(10f) }.onClick { rotate(false) }
+            imageButton(Buttons.RotateRight()) { cell -> cell.size(UI_BTN_SIZE).right().bottom().expand().colspan(2).padRight(10f).padBottom(10f).row() }.onClick { rotate(true) }
+
+            // move block left button
+            imageButton(Buttons.Arrow()) { cell -> cell.size(UI_BTN_SIZE).left().bottom().padLeft(10f).padBottom(10f) }.run {
+                isTransform = true
+                setOrigin(UI_BTN_SIZE * 0.5f, UI_BTN_SIZE * 0.5f)
+                rotateBy(180f)
+                onClick { moveLeft() }
+            }
+            // drop block button
+            imageButton(Buttons.Arrow()) { cell -> cell.size(UI_BTN_SIZE).expandX().right().bottom().padRight(10f).padBottom(10f) }.run {
+                isTransform = true
+                setOrigin(UI_BTN_SIZE * 0.5f, UI_BTN_SIZE * 0.5f)
+                rotateBy(270f)
+                onClick { moveBottom() }
+            }
+            // move block right button
+            imageButton(Buttons.Arrow()) { cell -> cell.size(UI_BTN_SIZE).right().bottom().padRight(10f).padBottom(10f).row() }.onClick { moveRight() }
+
+            background = skin.getDrawable(Drawables.GutterDark())
+            setFillParent(true)
+        })
     }
 
     private fun setLevel(newLevel: Int) {
         currentLevel = newLevel
+        levelLabel.txt = "Current Level: $currentLevel"
+
         // set color theme
         currentColorTheme = if (currentLevel >= 15) ColorTheme.Transparent else ColorTheme.Theme0
         if (currentLevel < 15) {
@@ -101,38 +144,47 @@ class GameScreen(context: Context) : KtxScreen {
         }
     }
 
-    override fun render(delta: Float) {
-        // render game
-        when {
-            Gdx.input.isKeyJustPressed(Input.Keys.LEFT) -> {
-                currentBlock.moveLeft(grid)
-                audioMgr.play(SoundAssets.BlockMove)
-                resetAccumulator()
-            }
-            Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) -> {
-                currentBlock.moveRight(grid)
-                audioMgr.play(SoundAssets.BlockMove)
-                resetAccumulator()
-            }
-            Gdx.input.isKeyJustPressed(Input.Keys.DOWN) -> {
-                currentBlock.moveToBottom(grid)
-                if (lockTimer == 0f) {
-                    lockTimer = MAX_LOCK_TIMER
-                }
-            }
-            Gdx.input.isKeyJustPressed(Input.Keys.R) -> {
-                currentBlock.rotate(grid, true)
-                audioMgr.play(SoundAssets.RotateRight)
-                resetAccumulator()
-            }
-            Gdx.input.isKeyJustPressed(Input.Keys.L) -> {
-                currentBlock.rotate(grid, false)
-                audioMgr.play(SoundAssets.RotateLeft)
-                resetAccumulator()
-            }
-            Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) -> game.setScreen<MenuScreen>()
-        }
+    private fun moveLeft() {
+        currentBlock.moveLeft(grid)
+        audioMgr.play(SoundAssets.BlockMove)
+        resetAccumulator()
+    }
 
+    private fun moveRight() {
+        currentBlock.moveRight(grid)
+        audioMgr.play(SoundAssets.BlockMove)
+        resetAccumulator()
+    }
+
+    private fun moveBottom() {
+        currentBlock.moveToBottom(grid)
+        if (lockTimer == 0f) {
+            lockTimer = MAX_LOCK_TIMER
+        }
+    }
+
+    private fun rotate(clockwise: Boolean) {
+        currentBlock.rotate(grid, clockwise)
+        audioMgr.play(if (clockwise) SoundAssets.RotateRight else SoundAssets.RotateLeft)
+        resetAccumulator()
+    }
+
+    override fun render(delta: Float) {
+        // desktop input
+        when {
+            Gdx.input.isKeyJustPressed(Input.Keys.LEFT) -> moveLeft()
+            Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) -> moveRight()
+            Gdx.input.isKeyJustPressed(Input.Keys.DOWN) -> moveBottom()
+            Gdx.input.isKeyJustPressed(Input.Keys.R) -> rotate(true)
+            Gdx.input.isKeyJustPressed(Input.Keys.L) -> rotate(false)
+        }
+        // game logic
+        updateLogic(delta)
+        // render game
+        draw(delta)
+    }
+
+    private fun updateLogic(delta: Float) {
         if (lockTimer == 0f) {
             // no block locking at the moment -> check if block needs to be moved down
             accumulator += delta
@@ -157,6 +209,14 @@ class GameScreen(context: Context) : KtxScreen {
                 }
             }
         }
+    }
+
+    private fun draw(delta: Float) {
+        // draw UI
+        stage.act(delta)
+        batch.color = Color.WHITE
+        stage.viewport.apply(true)
+        stage.draw()
 
         // render game
         viewport.apply(true)
@@ -166,16 +226,31 @@ class GameScreen(context: Context) : KtxScreen {
             for (row in 0 until grid.rows()) {
                 for (column in 0 until grid.columns()) {
                     batch.color = currentColorTheme.colorMap[grid[row, column].type]
-                    it.draw(blockTexture, column.toFloat() * 0.75f, row.toFloat() * 0.75f, 0.75f, 0.75f)
+                    it.draw(blockTexture, 1.4f + column * GAME_SCALE, 1.8f + row * GAME_SCALE, GAME_SCALE, GAME_SCALE)
                 }
             }
         }
 
-        // draw UI
-        stage.act(delta)
+        // render block preview
+        batch.projectionMatrix = stage.viewport.camera.combined
+        batch.use {
+            if (currentColorTheme == ColorTheme.Transparent) {
+                batch.color = Color.WHITE
+            } else {
+                batch.color = currentColorTheme.colorMap[currentBlock.next()]
+            }
+            val pattern = currentBlock.next().patterns[0]
+            val offsetY = if (pattern.size == 4) 904f else 892f
+            for (row in 0 until pattern.size) {
+                for (col in 0 until pattern[0].size) {
+                    if (pattern[row][col] == 0) continue
+                    it.draw(blockTexture, 180f + PREVIEW_BLOCK_SIZE * col, offsetY - PREVIEW_BLOCK_SIZE * row, PREVIEW_BLOCK_SIZE, PREVIEW_BLOCK_SIZE)
+                }
+            }
+        }
+
+        // reset batch color
         batch.color = Color.WHITE
-        stage.viewport.apply(true)
-        stage.draw()
     }
 
     private fun getPointsForClear(numClearedRows: Int): Int {
@@ -188,13 +263,18 @@ class GameScreen(context: Context) : KtxScreen {
         }
     }
 
+    private fun updateHighscore(newHighscore: Int) {
+        highscore = newHighscore
+        highscoreLabel.txt = "Highscore: $highscore"
+    }
+
     private fun lockBlock() {
         // check for cleared rows
         val numClearedRows = grid.removeFullRows(currentBlock)
         if (numClearedRows > 0) {
             // rows cleared -> update highscore and level
             clearedRows += numClearedRows
-            highscore += getPointsForClear(numClearedRows)
+            updateHighscore(highscore + getPointsForClear(numClearedRows))
             audioMgr.play(SoundAssets.LineComplete)
 
             if (clearedRows >= ROWS_FOR_NEXT_LEVEL) {
