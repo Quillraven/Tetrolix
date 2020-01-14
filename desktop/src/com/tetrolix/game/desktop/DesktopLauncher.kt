d617f1d07f5ec5211f80a6d02a9a47ae5f4911df
Quillraven <quillraven@gmail.com>
2020-01-14 19:00:02 +0100

    updated to libgdx 1.9.10, libktx 1.9.10-b3 and kotlin 1.3.61
    - updated android plugin and gradle wrapper
    - updated DesktopLauncher to follow kotlin standards

--- desktop/src/com/tetrolix/game/desktop/DesktopLauncher.kt
+++ desktop/src/com/tetrolix/game/desktop/DesktopLauncher.kt
@@ -1,16 +1,14 @@
 package com.tetrolix.game.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.tetrolix.game.Tetrolix
 
-object DesktopLauncher {
-    @JvmStatic
-    fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration().apply {
-            width = 504
-            height = 896
-        }
-        LwjglApplication(Tetrolix(), config)
+
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration().apply {
+        setWindowSizeLimits(504, 896, -1, -1)
+        setTitle("Tetrolix")
     }
+    Lwjgl3Application(Tetrolix(), config)
 }
